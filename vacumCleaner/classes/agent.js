065d1f3439aa45ed89db92971a8bd809be0d31df
LSanchez08 <luis.alberto.sanchez.rios@live.com.mx>
2022-09-13 21:50:30 -0500

    First itter

--- vacumCleaner/classes/agent.js
+++ vacumCleaner/classes/agent.js
@@ -1,38 +1,116 @@
 // Finds "false" means area is clean
+const generateMapOverlay = (amount) => {
+	const initial = Array.from({ length: amount });
+	return initial.reduce((acc, element) => {
+		return [...acc, Array.from({ length: amount }).map(() => '')];
+	}, []);
+};
 
-const generateMap = (enviroment) => {
+const directions = {
+  north: {
+    x: 0,
+    y: -1
+  },
+  south: {
+    x: 0,
+    y: 1
+  },
+  east: {
+    x: 1,
+    y: 0
+  },
+  west: {
+    x: -1,
+    y: 0
+  }
+};
+
+const moveTo = Object.keys(directions);
+
+const generateMap = (enviroment, x = -1, y = -1) => {
   return enviroment.map((row) => {
+    y += 1;
     return row.map((spot) => {
-      return { clean: spot, visited: false };
-    })
-  })
+      if (x >= enviroment.length - 1) {
+        x = 0;
+      } else {
+        x += 1;
+      }
+      const notPossibleDirection = [];
+      if (y === 0) {
+        notPossibleDirection.push('north');
+      } else if (y >= enviroment.length - 1) {
+        notPossibleDirection.push('south');
+      }
+      if (x === 0) {
+        notPossibleDirection.push('west');
+      } else if (x >= enviroment.length - 1) {
+        notPossibleDirection.push('east');
+      }
+
+      return { clean: spot, exitedDirections: [] , notPossibleDirection };
+    });
+  });
 };
 
 class VacumCleaner {
-  constructor(map) {
+  constructor(map, size) {
+    this.overlay = generateMapOverlay(size);
+    this.mapa = map;
     this.finds = 0;
     this.enviroment = generateMap(map);
+    this.direction = 0;
     this.x = 0;
     this.y = 0;
-    this.direction = {
-      x: 1,
-      y: 0
-    }
+    this.changeDir = true;
   };
 
   getAction() {
     if (this.finds) {
+      this.enviroment[this.y][this.x].clean = 0;
       return 'clean';
     } else {
-      return 'right';
+      return 'move';
     }
   };
 
   setPerception() {
-    this.finds = this.enviroment[this.x][this.y];
-    
-    this.getAction();
+    this.overlay[this.y][this.x] = 'X';
+    this.finds = this.mapa[this.y][this.x];
+
+    return this.getAction();
   };
+
+  move() {
+    const location = this.enviroment[this.y][this.x];
+    if (location.notPossibleDirection.includes(moveTo[this.direction])) {
+      while (location.notPossibleDirection.includes(moveTo[this.direction])){
+        this.direction += 1
+        if (this.direction >= 4) {
+          this.direction = 0;
+        } else if (this.direction <= -1) {
+          this.direction = 3;
+        }
+        const allDirections = [...location.exitedDirections, ...location.notPossibleDirection];
+        if (allDirections.includes(moveTo[this.direction]) && allDirections.length < 4) {
+          this.direction = moveTo.indexOf(moveTo.find((dir) => !allDirections.includes(dir)));
+        }
+      }
+    }
+    this.overlay[this.y][this.x] = '';
+    if (!location.exitedDirections.includes(moveTo[this.direction])) {
+      location.exitedDirections.push(moveTo[this.direction]);
+    }
+    if (location.exitedDirections === 2) {
+      location.exitedDirections = [];
+    }
+    // if (this.x == 0 && this.y == 0) {
+    //   console.log({direction: this.direction, x: this.x, y: this.y})
+    // }
+    this.x += directions[moveTo[this.direction]].x;
+    this.y += directions[moveTo[this.direction]].y;
+    this.overlay[this.y][this.x] = 'X';
+  }
 };
 
 module.exports = VacumCleaner;
--- vacumCleaner/index.js
+++ vacumCleaner/index.js
@@ -3,20 +3,44 @@ const VacumCleaner = require('./classes/agent');
 
 const generateMap = (amount) => {
 	const initial = Array.from({ length: amount });
-
 	return initial.reduce((acc, element) => {
 		return [...acc, Array.from({ length: amount }).map(() => Math.round(Math.random()))];
 	}, []);
 };
 
-const main = () => {
-	const map = generateMap(2);
+const testEnd = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].includes(1)) {
+			return true;
+		}
+	}
+	return false;
+};
 
-	const agent = new VacumCleaner(map);
-
-	console.log(agent.enviroment)
+const main = (size) => {
+	const map = generateMap(size);
+	const agent = new VacumCleaner(map, size);
+	console.log(agent.overlay);
+	console.log(map)
+	let val = 0
+	while (testEnd(map)) {
+		val += 1
+		const action = agent.setPerception();
+		if (action === 'clean') {
+			map[agent.y][agent.x] = 0;
+		} else {
+			agent.move();
+		}
+		
+		// console.log(action);
+		console.log(map)
+		// console.log(agent.overlay);
+	}
+	
+	
+	// console.log(agent.enviroment)
 };
 
 
-main()
+main(3);
 
